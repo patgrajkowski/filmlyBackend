b0a2c73edb21f377678f533af1e5fa6241c9b588
patgrajkowski <patgrajkowski@gmail.com>
2022-01-07 19:40:49 +0100

    removed cors package

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
-app.use(cors());
-app.options('*', cors());
 mongoose
   .connect(
     'mongodb://patgrajkowski:sjV7q93CQgBzTvQc@cluster0-shard-00-00.v1blu.mongodb.net:27017,cluster0-shard-00-01.v1blu.mongodb.net:27017,cluster0-shard-00-02.v1blu.mongodb.net:27017/wypozyczalnia?ssl=true&replicaSet=atlas-6mm1sf-shard-0&authSource=admin&retryWrites=true&w=majority'
