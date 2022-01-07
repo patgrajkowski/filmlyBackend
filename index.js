d095f428b801d71865b501c5ef1462959abc114e
patgrajkowski <patgrajkowski@gmail.com>
2022-01-07 19:17:59 +0100

    test FIX

--- index.js
+++ index.js
@@ -10,14 +10,12 @@ const app = express();
 
 app.use(cors());
 app.options('*', cors());
-
 mongoose
   .connect(
-    'mongodb+srv://patgrajkowski:V6uxJ4VnrQtq2rZ@cluster0.v1blu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    'mongodb://patgrajkowski:sjV7q93CQgBzTvQc@cluster0-shard-00-00.v1blu.mongodb.net:27017,cluster0-shard-00-01.v1blu.mongodb.net:27017,cluster0-shard-00-02.v1blu.mongodb.net:27017/wypozyczalnia?ssl=true&replicaSet=atlas-6mm1sf-shard-0&authSource=admin&retryWrites=true&w=majority'
   )
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.error('Could not connect to MongoDB...'));
-
 app.use(express.json());
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
